fe9b9c4971ace34648ea696ab0e38a0fae3ddd26
Harshvardhan Singh <hvsreal.223@gmail.com>
2022-08-04 10:28:32 +0530

    prod url

--- next.config.js
+++ next.config.js
@@ -7,7 +7,7 @@
 // module.exports = nextConfig;
 module.exports = {
   env: {
-    baseUrl: "http://localhost:3001",
-    // baseUrl: "https://morefined.com",
+    // baseUrl: "http://localhost:3001",
+    baseUrl: "http://34.229.110.181",
   },
 };
